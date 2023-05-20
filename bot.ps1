550515f3439b83d5eb31a804f492fbb645f93692
Desgasa <122233531+Desgasa@users.noreply.github.com>
2023-05-20 13:26:48 +0300

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -4,6 +4,6 @@ $json = Invoke-RestMethod -Uri "https://api.telegram.org/botToken/getUpdates"
  $timed = Get-Date -UFormat %s
  $times = $timed - $timem
 if($times -gt 4500 -and $times -lt 5400)
- {Invoke-RestMethod -Uri "https://api.telegram.org/botToken/sendMessage?chat_id=5093926555&text=Anybody Her?"}
+ {Invoke-RestMethod -Uri "https://api.telegram.org/botToken/sendMessage?chat_id=yourchatid&text=Anybody Her?"}
 elseif ($times -gt 5400) {
-Invoke-RestMethod -Uri "https://api.telegram.org/botToken/sendMessage?chat_id=-1001648382622&text=Hello"}
+Invoke-RestMethod -Uri "https://api.telegram.org/botToken/sendMessage?chat_id=yourchatid&text=Hello"}
